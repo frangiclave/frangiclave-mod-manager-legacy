74ab163f5497db0551ea44a38a76314e6295ab22
Lyrositor <gagne.marc@gmail.com>
2018-08-30 17:37:25 +0200

    Version 0.2.3

--- ci/install.sh
+++ ci/install.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-source ~/.cargo/env || true
-
 if [ $TRAVIS_OS_NAME = linux ]
 then
-    sudo apt-get -yqq curl install build-essential gcc git libssl-dev mono-devel nuget unzip wget
+    sudo apt-get install -qq build-essential curl gcc git libssl-dev mono-devel nuget unzip wget
 else
-    brew install mono
+    brew install git mono nuget openssl unzip
 fi
 
 rustup self update
